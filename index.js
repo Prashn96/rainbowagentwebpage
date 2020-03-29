0a40476b03d86b696fb6eab0fe17025bd6f05446
Prashn96 <49476736+Prashn96@users.noreply.github.com>
2020-03-29 23:30:22 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,155 @@
+/* Chose one of the import statement below */
+import rainbowSDK from "./rainbow-sdk.min.js";
+
+// const rainbowSDK = require('rainbow-web-sdk');
+// If you use the bundler (for example - Webpack)
+
+const chatArea = document.getElementById("chatarea");
+const sendArea = document.getElementById("sendchatarea");
+const sendMessageBtn = document.getElementsByClassName("sendbutton")[0];
+const categoryDropdown = document.getElementsByClassName("dropdownlist")[0];
+const requestButton = document.getElementsByClassName("requestbutton")[0];
+const agentStatusText = document.getElementsByClassName("agent_status")[0];
+let agent_id;
+let agent_name;
+let guest_id;
+let convo;
+let message;
+
+const onReady = async () => {
+  sendMessageBtn.addEventListener("click", sendClick, false);
+  requestButton.addEventListener("click", requestClick, false);
+
+  var myRainbowLogin = "jason_chow@mymail.sutd.edu.sg"; // Replace by your login
+  var myRainbowPassword = "Rainbow1!"; // Replace by your password
+  var timeToLive = 600;
+
+  // The SDK for Web is ready to be used, so you can sign in
+  let account = await rainbowSDK.connection.signin(
+    myRainbowLogin,
+    myRainbowPassword
+  );
+  console.log(rainbowSDK.admin);
+  // Successfully signed to Rainbow and the SDK is started completely. Rainbow data can be retrieved.
+  /*rainbowSDK.admin.createAnonymousGuestUser(timeToLive).then((guest_user)=> {
+              console.log(guest_user);
+          }).catch(err=>{console.log(err);})*/
+
+  // rainbowSDK.contacts
+  //   .searchById("5e78cfb00beb4e6ae713d999 ")
+  //   .then(add => {
+  //     console.log("searched");
+  //     console.log(add);
+  //     return rainbowSDK.contacts.addToNetwork(add);
+  //   })
+  //   .then(contact => {
+  //     console.log(contact);
+  //     return rainbowSDK.conversations.openConversationForContact(contact);
+  //   })
+  //   .then(conv => {
+  //     console.log(conv);
+  //     return rainbowSDK.im.sendMessageToConversation(conv, "Test2");
+  //   })
+  //   .then(obj => console.log(obj))
+  //   .catch(err => {});
+};
+
+var onLoaded = function onLoaded() {
+  console.log("[Hello World] :: On SDK Loaded !");
+
+  rainbowSDK
+    .initialize(
+      "843d81a06c2311eaa8fbfb2c1e16e226",
+      "0iV19xt6OdGoQb2N9V1Evp1KKkOV9FHRIn8c9bEOFixcwzKwDB3xWAGp1MVmWEkg"
+    )
+    .then(() => {
+      console.log("[Hello World] :: Rainbow SDK is initialized!");
+      /*
+            rainbowSDK.contacts.searchById(id)
+            .then((contact) => {
+                console.log(contact);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+            */
+    })
+    .catch(err => {
+      console.log("[Hello World] :: Something went wrong with the SDK.", err);
+    });
+};
+
+const sendClick = () => {
+  // chatArea.innerHTML += `You: \n ${sendArea.value} \n\n`;
+  // sendArea.value = "";
+};
+
+const requestClick = () => {
+  // TODO: Add http call to request for agent
+  console.log("Test axios");
+
+  const apiUrl =
+    "http://localhost:3030/agent/checkforrequest/5e7a32e50beb4e6ae713daaf";
+  const body = {};
+  // axios
+  //   .get(apiUrl)
+  //   .then((err, rs) => {
+  //     if (err) {
+  //       console.error(err.message);
+  //       return;
+  //     }
+  //     console.log(rs.data);
+  //   })
+  //   .catch(err => {});
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    console.log("wow");
+    if (this.readyState == 4 && this.status == 200) {
+      // Typical action to be performed when the document is ready:
+      console.log(xhttp.response);
+      const obj = JSON.parse(xhttp.response);
+      console.log((guest_id = obj.suppReq.guest_id));
+      console.log((name = obj.suppReq.name));
+      console.log((agent_id = obj.suppReq.agent_id));
+      console.log((agent_name = obj.suppReq.agent_name));
+      rainbowSDK.contacts
+        .searchById(guest_id)
+        .then(contact => {
+          console.log(contact);
+          return rainbowSDK.conversations.openConversationForContact(contact);
+        })
+        .then(conv => {
+          convo = conv;
+          console.log(conv);
+          return rainbowSDK.im.sendMessageToConversation(conv, "Test2");
+        })
+        .then(obj => {
+          console.log(obj);
+          document.addEventListener(
+            rainbowSDK.conversations.RAINBOW_ONCONVERSATIONCHANGED,
+            message => {
+              agentMessage(message);
+            }
+          );
+        })
+        .catch(err => {});
+    }
+    if (this.readyState == 4 && this.status >= 400) {
+      setTimeout(() => {
+        requestClick();
+      }, 5000);
+    }
+  };
+  xhttp.open("GET", apiUrl, true);
+  xhttp.send();
+};
+
+const agentMessage = message => {
+  chatArea.innerHTML += `Agent: \n ${message} \n\n`;
+};
+
+document.addEventListener(rainbowSDK.RAINBOW_ONREADY, onReady);
+
+document.addEventListener(rainbowSDK.RAINBOW_ONLOADED, onLoaded);
+rainbowSDK.start();
+rainbowSDK.load();
