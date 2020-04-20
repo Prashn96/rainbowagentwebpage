782035bc917051d838eb7a97d1c87d48e89cb0a4
Prashn96 <49476736+Prashn96@users.noreply.github.com>
2020-04-20 17:41:59 +0800

    .js file for login page

--- index.js
+++ index.js
@@ -1,223 +1,27 @@
-/* Chose one of the import statement below */
-import rainbowSDK from "./rainbow-sdk.min.js";
+let agentId;
 
-// const rainbowSDK = require('rainbow-web-sdk');
-// If you use the bundler (for example - Webpack)
+function validate() {
+  var username = document.getElementById("userid");
+  var password = document.getElementById("pswrd");
+  console.log("leggo");
+  localStorage.setItem("username", username.value);
+  localStorage.setItem("password", password.value);
 
-const chatArea = document.getElementById("chatarea");
-const sendArea = document.getElementById("sendchatarea");
-// const loginBtn = document.getElementsByTagName("bttn")[0];
-const sendMessageBtn = document.getElementsByClassName("sendbutton")[0];
-const categoryDropdown = document.getElementsByClassName("dropdownlist")[0];
-const agentDropdown = document.getElementsByClassName("dropdownlistagent")[0];
-const requestButton = document.getElementsByClassName("requestbutton")[0];
-var availList = document.getElementsByClassName("availagent")[0];
-const customerStatusText = document.getElementsByClassName(
-  "customer_status"
-)[0];
-const quitBtn = document.getElementsByClassName("quitbutton")[0];
-let agent_id;
-let agent_name;
-let guest_id;
-let convo;
-let message;
+  username = localStorage.getItem("username");
+  password = localStorage.getItem("password");
 
-const agentMaps = {
-  "Jason2 Chow2": "5e7a32e50beb4e6ae713daaf",
-  "Shaozuo Zhang": "5e78cfb00beb4e6ae713d999",
-};
-
-const onReady = async () => {
-  // loginBtn.addEventListener("click", loginClick, false);
-  // sendMessageBtn.addEventListener("click", sendClick, false);
-  requestButton.addEventListener("click", requestClick, false);
-  quitBtn.addEventListener("click", closeConvoNetwork, false);
-  availList.addEventListener("change", changeData, false);
-
-  var myRainbowLogin = "jason_chow@mymail.sutd.edu.sg"; // Replace by your login
-  var myRainbowPassword = "Rainbow1!"; // Replace by your password
-  var timeToLive = 600;
-
-  // The SDK for Web is ready to be used, so you can sign in
-  let account = await rainbowSDK.connection.signin(
-    myRainbowLogin,
-    myRainbowPassword
-  );
-  console.log(rainbowSDK.admin);
-};
-
-var onLoaded = function onLoaded() {
-  console.log("[Hello World] :: On SDK Loaded !");
-
-  rainbowSDK
-    .initialize(
-      "843d81a06c2311eaa8fbfb2c1e16e226",
-      "0iV19xt6OdGoQb2N9V1Evp1KKkOV9FHRIn8c9bEOFixcwzKwDB3xWAGp1MVmWEkg"
-    )
-    .then(() => {
-      console.log("[Hello World] :: Rainbow SDK is initialized!");
-
-      //calls changeData() function once agents login(agent availability)
-      changeData();
-      //calls pollForCustomer() function once agent login
-      pollForCustomer();
-
-      /*
-            rainbowSDK.contacts.searchById(id)
-            .then((contact) => {
-                console.log(contact);
-            })
-            .catch(err => {
-                console.log(err);
-            });
-            */
-    })
-    .catch((err) => {
-      console.log("[Hello World] :: Something went wrong with the SDK.", err);
-    });
-};
-// const loginClick = () => {
-//   changeData();
-//   pollForCustomer();
-// };
-const changeData = () => {
-  if (availList.value == "available") {
-    alert("Agent is available!");
-    console.log("Agent available");
-  } else if (availList.value == "busy") {
-    alert("Agent is busy!");
-    console.log("Agent busy");
+  if (username == "jason_chow@mymail.sutd.edu.sg" && password == "Rainbow1!") {
+    localStorage.setItem("agentId", "Jason2 Chow2");
+    console.log("valid");
+    window.open("index2.html");
+  } else if (
+    username == "zsz2628836781@gmail.com" &&
+    password == "SUTDzsz19991226"
+  ) {
+    localStorage.setItem("agentId", "Shaozuo Zhang");
+    console.log("valid");
+    window.open("index2.html");
   } else {
-    console.log("Finding agent");
+    alert("Incorrect Username/Password!");
   }
-};
-const sendClick = () => {
-  const toSend = sendArea.value;
-  sendArea.value = "";
-  agentMessage(toSend);
-  rainbowSDK.im.sendMessageToConversation(convo, toSend);
-};
-
-const requestClick = () => {
-  pollForCustomer(agentDropdown.value);
-};
-
-const updateCustomerStatusText = (customer) => {
-  customerStatusText.innerHTML += customer;
-};
-
-const pollForCustomer = (agentId) => {
-  // TODO: Add http call to request for agent
-
-  const apiUrl = `http://13.76.87.194:3030/common/reqstatus?agentId=${agentId}`;
-  const body = {};
-  // axios
-  //   .get(apiUrl)
-  //   .then((err, rs) => {
-  //     if (err) {
-  //       console.error(err.message);
-  //       return;
-  //     }
-  //     console.log(rs.data);
-  //   })
-  //   .catch(err => {});
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    console.log("wow");
-    if (this.active == true && this.status == 200) {
-      // Typical action to be performed when the document is ready:
-      console.log("IS ACTIVE1");
-      console.log(xhttp.response);
-      const obj = JSON.parse(xhttp.response);
-      console.log((guest_id = obj.support_req.guestId));
-      console.log((name = obj.support_req.name));
-      console.log((agent_id = obj.support_req.agentId));
-      console.log((agent_name = obj.support_req.agentName));
-      updateCustomerStatusText(name);
-      //enable send message button only when connecting/connected
-      sendMessageBtn.addEventListener("click", sendClick, false);
-      rainbowSDK.contacts
-        .searchById(guest_id)
-        .then((contact) => {
-          console.log(contact);
-          return rainbowSDK.conversations.openConversationForContact(contact);
-        })
-        .then((conv) => {
-          convo = conv;
-          console.log(conv);
-          return rainbowSDK.im.sendMessageToConversation(
-            conv,
-            "Agent has connected"
-          );
-        })
-        .then((obj) => {
-          console.log(obj);
-          document.addEventListener(
-            rainbowSDK.im.RAINBOW_ONNEWIMMESSAGERECEIVED,
-            (msg, conv, cc) => {
-              clientMessage(extractMessage(msg));
-            }
-          );
-        })
-        .catch((err) => {});
-    }
-
-    if (this.active == true && this.status >= 400) {
-      console.log("IS ACTIVE2");
-      //enable send message button only when connecting/connected
-      sendMessageBtn.addEventListener("click", sendClick, false);
-
-      // setTimeout(() => {
-      //   pollForCustomer();
-      // }, 5000);
-    } else {
-      console.log("NOT ACTIVE");
-      setTimeout(() => {
-        pollForCustomer();
-      }, 5000);
-    }
-  };
-
-  xhttp.open("GET", apiUrl, true);
-  xhttp.send();
-};
-
-// const loginFunction = () => {
-//   changeData();
-//   pollForCustomer();
-// };
-
-const closeConvoNetwork = (reqId) => {
-  const apiUrl = `http://13.76.87.194:3030/common/closereq/${reqId}`;
-  clearInterval(checkIntervalTimer);
-  fetch(apiUrl)
-    .then((response) => response.text())
-    .then((htmlText) => {
-      console.log(htmlText);
-    })
-    .catch(console.error)
-    .finally(() => closeConvo());
-};
-
-const closeConvo = () => {
-  // TODO: Close convo
-  console.log("Closing convo");
-};
-
-const clientMessage = (message) => {
-  chatArea.innerHTML += `Client: \n ${message} \n\n`;
-};
-
-const agentMessage = (message) => {
-  chatArea.innerHTML += `Agent: \n ${message} \n\n`;
-};
-
-const extractMessage = (msg) => {
-  return msg.detail.message.data;
-};
-
-document.addEventListener(rainbowSDK.RAINBOW_ONREADY, onReady);
-
-document.addEventListener(rainbowSDK.RAINBOW_ONLOADED, onLoaded);
-rainbowSDK.start();
-rainbowSDK.load();
+}
